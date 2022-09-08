9ef2492a5bc5fe3b327b15d674f61c4ca1a6977d
Jasper Maraño <57208299+Clerudx@users.noreply.github.com>
2022-09-08 14:17:28 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Student jasper = new Student("Marano", "Jasper Eric", "D", 10, 01, 2003, 2022103266, "maranojd@students.national-edu.ph", true);
+
+        jasper.sayMyStudentNumebr();
+        jasper.sayMyEmailAddress();
+        jasper.amIAwesome();
+    }
+}
